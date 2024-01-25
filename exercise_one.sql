f88edf796ff269499468c746cfe67ce462955961
Topaz712 <tzam712@gmail.com>
2024-01-25 13:54:34 -0600

    exercises in seperate files but combined now

--- exercise_one.sql
+++ exercise_one.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Customers (
-    CustomerID INT,
-    Name TEXT,
-    Email TEXT
+  CustomerID INT,
+  Name TEXT,
+  Email TEXT
 );
 
 INSERT INTO Customers (CustomerID, Name, Email)
@@ -10,4 +10,13 @@ VALUES
   (2, 'Bob Smith', 'bob.smith@email.com'),
   (3, 'Charlie Brown', 'charlie.brown@email.com');
 
-  SELECT * FROM Customers;
+-- exercise two --
+UPDATE Customers
+SET Email = 'bob.smith@newdomain.com'
+WHERE CustomerID = 2;
+
+-- exercise three --
+DELETE FROM Customers
+WHERE CustomerID = 3;
+
+SELECT * FROM Customers;
